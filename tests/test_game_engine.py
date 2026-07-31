601a569d115929accf02f398446172416194a2be
Connor W <cwilloughby311@gmail.com>
2026-07-31 14:14:46 +0100

    chores: linting

--- tests/test_game_engine.py
+++ tests/test_game_engine.py
@@ -6,9 +6,9 @@ import unittest
 class TestGameEngine(unittest.TestCase):
     """Stores tests for the core game engine"""
 
+    @unittest.skip("Scaffolding for the short term")
     def test_init(self):
-        """scaffolding"""
-
+        """Scaffolding"""
         return NotImplemented
 
 
